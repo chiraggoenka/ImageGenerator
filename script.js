b98411e3e286b7074c79f49d3284e30d4476a224
Chirag Goenka <chirag2780@gmail.com>
2024-11-12 23:59:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const accessKey= 'Cp5Zy622IOZbZYhKjDJjsyDf9-jupPEX2ClK0PPrxE';
+const accessKey= "Cp5Zy622IOZbZYhKjDJjsyDf9-jupPEX2ClK0PPrxE";
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
